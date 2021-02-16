2750d926b3ac7c150608e5e123d222de0e637d40
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-16 18:39:48 +0545

    refactored to stateful widget with some style changes

--- lib/widgets/add_transaction.dart
+++ lib/widgets/add_transaction.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class AddTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final priceController = TextEditingController();
-
+class AddTransaction extends StatefulWidget {
   final Function onPressedFunc;
 
   AddTransaction({this.onPressedFunc});
+
+  @override
+  _AddTransactionState createState() => _AddTransactionState();
+}
+
+class _AddTransactionState extends State<AddTransaction> {
+  final titleController = TextEditingController();
+
+  final priceController = TextEditingController();
 
   void submit() {
     final inputTitle = titleController.text;
@@ -16,55 +22,68 @@ class AddTransaction extends StatelessWidget {
       return;
     }
 
-    onPressedFunc(
+    widget.onPressedFunc(
       inputTitle,
       inputPrice,
     );
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: (_) => submit(),
-              controller: titleController,
-              decoration: InputDecoration(
-                labelText: 'Expense Name',
-                hintText: 'E.g. Shoes',
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Add a new transaction',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+          TextField(
+            onSubmitted: (_) => submit(),
+            controller: titleController,
+            decoration: InputDecoration(
+              labelText: 'Expense Name',
+              hintText: 'E.g. Shoes',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
               ),
             ),
-            TextField(
-              onSubmitted: (_) => submit(),
-              controller: priceController,
-              decoration: InputDecoration(
-                labelText: 'Price',
-                hintText: 'E.g. 69.99',
-              ),
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
+          ),
+          TextField(
+            onSubmitted: (_) => submit(),
+            controller: priceController,
+            decoration: InputDecoration(
+              labelText: 'Price',
+              hintText: 'E.g. 69.99',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
               ),
             ),
-            FlatButton(
+            keyboardType: TextInputType.numberWithOptions(
+              decimal: true,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 20,
+            ),
+            child: FlatButton(
               onPressed: submit,
               child: Text('Add Expense'),
               color: Colors.blue,
               textColor: Colors.white,
-            )
-          ],
-          crossAxisAlignment: CrossAxisAlignment.end,
-        ),
-      ),
-      elevation: 5,
-      margin: EdgeInsets.only(
-        left: 16,
-        right: 16,
-        top: 10,
-        bottom: 10,
+            ),
+          )
+        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
       ),
     );
   }
