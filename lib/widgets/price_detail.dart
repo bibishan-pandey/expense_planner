54d3c240004c0916bcf42786aa34146b61c9687d
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-16 17:53:06 +0545

    extracting price detail widget

---
+++ lib/widgets/price_detail.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class PriceDetail extends StatelessWidget {
+  final double amount;
+
+  const PriceDetail({
+    Key key,
+    @required this.amount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        '\$$amount',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      margin: EdgeInsets.only(right: 10),
+      padding: EdgeInsets.only(
+        top: 20,
+        bottom: 20,
+        left: 10,
+        right: 10,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(3),
+          bottomLeft: Radius.circular(3),
+        ),
+        color: Colors.blue,
+        gradient: SweepGradient(colors: [
+          Colors.lightBlueAccent,
+          Colors.lightBlue,
+          Colors.lightBlueAccent
+        ], stops: [
+          0.0,
+          0.5,
+          1.0
+        ]),
+      ),
+    );
+  }
+}
