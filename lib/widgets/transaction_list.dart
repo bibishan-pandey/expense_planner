955f6f50c0b7ef8ee695afaad9b4c9e6b0582f43
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-16 22:18:29 +0545

    added no transaction image for null

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,16 +11,38 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, idx) {
-          return ExpenseCard(
-            amount: transactions[idx].amount,
-            title: transactions[idx].title,
-            dateTime: transactions[idx].dateTime,
-          );
-        },
-        itemCount: transactions.length,
-      ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, idx) {
+                return ExpenseCard(
+                  amount: transactions[idx].amount,
+                  title: transactions[idx].title,
+                  dateTime: transactions[idx].dateTime,
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
